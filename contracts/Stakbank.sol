524031832d15c4dc6feee2cfe3260546ffd94dab
ngot23 <levuquang2k1@gmail.com>
2021-02-06 23:51:53 +0700

    init project

---
+++ contracts/Stakbank.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.6.0 <0.8.0;
+
+import "./libraries/ERC20.sol";
+import "./libraries/SafeMath.sol";
+import "./libraries/Ownable.sol";
+
+contract StakbankTest is ERC20, Ownable {
+    using SafeMath for uint;
+
+    
+
+}
