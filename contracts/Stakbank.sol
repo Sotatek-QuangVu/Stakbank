7123d80c321f2364ed73723076e427d8536d96f0
ngot23 <levuquang2k1@gmail.com>
2021-02-07 11:24:49 +0700

    complete contract but not even testing

--- contracts/Stakbank.sol
+++ contracts/Stakbank.sol
@@ -13,8 +13,10 @@ contract StakbankTest0 is ERC20, Ownable {
     
     uint private _validCoinNum;
     uint private _periodTime; // minimum time to trigger distribute and time to be valid coin
-    uint private _cummEth; // cumulative Eth value per JST
     uint public lastDis; // last reward distribution
+    uint public feePercent;
+
+    uint private _cummEth; // cumulative Eth value per JST
     
     struct Transaction {
         address staker;
@@ -23,11 +25,19 @@ contract StakbankTest0 is ERC20, Ownable {
     }
     Transaction[] private stakingTrans;
     
+    struct Detail {
+        uint _cummEth;
+        uint coin;
+    }
+    mapping(address => Detail[]) private _eStaker;
+
     constructor(uint _totalSupply) ERC20("JST Test0", "JST T0", _totalSupply) public {
         owner = msg.sender;
         _periodTime = 120 seconds;
         lastDis = block.timestamp;
+        feePercent = 8;
     }
+
 
     //-------------------helper func-------------------/
     function isHolder(address add) private view returns (bool) {
@@ -42,10 +52,11 @@ contract StakbankTest0 is ERC20, Ownable {
         _isStakeHolder[add] = false;
     }
 
-    function transferForStaking(address sender, address receiver, uint amount) private {
+    function transferForStaking(address sender, address recipient, uint amount) private {
         _balances[sender] = _balances[sender].sub(amount);
-        _balances[receiver] = _balances[receiver].add(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
     }
+
 
     //-------------------staking--------------------/
     function stake(uint amount) public {
@@ -59,10 +70,53 @@ contract StakbankTest0 is ERC20, Ownable {
 
     function unstake() public {
         require(isHolder(msg.sender));
+        withdrawReward();
         removeHolder(msg.sender);
         transferForStaking(address(this), msg.sender, _staking[msg.sender]);
     }
     
 
+    //-------------------reward-------------------/
+    function rewardDistribution() public {
+        uint cur = block.timestamp;
+        require(cur - lastDis >= 120 seconds);
 
+        for(uint i = 0; i < stakingTrans.length; i++) {
+            Transaction memory t = stakingTrans[i];
+            if (cur - t.time >= 120 seconds) {
+                _validCoinNum = _validCoinNum.add(t.coin);
+            } else {
+                Detail memory detail = Detail(_cummEth, t.coin);
+                _eStaker[t.staker].push(detail);
+            }
+        }
+
+        uint ethToReward = address(this).balance;
+        _cummEth = _cummEth.add(ethToReward.div(_validCoinNum));
+        for(uint i = 0; i < stakingTrans.length; i++) {
+            Transaction memory t = stakingTrans[i];
+            if (cur - t.time < 120 seconds) break;
+            Detail memory detail = Detail(_cummEth, t.coin);
+            _eStaker[t.staker].push(detail);
+        }
+
+        delete stakingTrans;
+    }
+
+    function withdrawReward() public {
+        require(isHolder(msg.sender));
+        uint userReward = 0;
+        for(uint i = 0; i < _eStaker[msg.sender].length; i++) {
+            Detail memory detail = _eStaker[msg.sender][i];
+            uint addEth = detail.coin.mul(_cummEth - detail._cummEth);
+            userReward = userReward.add(addEth);
+        }
+        uint fee = userReward.mul(feePercent).div(100);
+
+        address payable staker = address(uint(address(msg.sender)));
+        address payable platfromOwner = address(uint(address(owner)));
+
+        staker.transfer(userReward - fee);
+        platfromOwner.transfer(fee);
+    } 
 }
---
+++ scripts/ezScript.js
@@ -0,0 +1,21 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log(`deploying with the account ${deployer.address}...`);
+
+    const balance = await deployer.getBalance();
+    console.log(`balance of deployer: ${balance.toString()}`);
+
+    const Bank = await ethers.getContractFactory('Stakbank');
+    const Stakbank = Bank.deploy();
+    console.log(`my contract address: ${Stakbank.address}`);
+    
+}
+
+main()
+.then(() => process.exit(0))
+.catch((err) => {
+    console.log(err);
+    process.exit(1);
+});
