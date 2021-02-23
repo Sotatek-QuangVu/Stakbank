15d5fb0042f1a07b5382e7a0f1cf90db1d31c1a3
ngot23 <levuquang2k1@gmail.com>
2021-02-23 11:21:34 +0700

    erase not need in JST

--- contracts/JST.sol
+++ contracts/JST.sol
@@ -5,18 +5,9 @@ import "./ERC20.sol";
 import "./Ownable.sol";
 
 contract JST is ERC20, Ownable {
-    address public StakBank;
 
     constructor(uint _totalSupply) ERC20("Jig Stack", "JST", _totalSupply) {
-        StakBank = address(0);
+    
     }
 
-    function verifyStakBank(address bank) external onlyOwner {
-        StakBank = bank;
-    }
-
-    modifier onlyStakBank() {
-        require(StakBank == msg.sender);  
-        _;
-    }
 }
