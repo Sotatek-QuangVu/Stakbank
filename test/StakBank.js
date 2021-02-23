1e49ac569e103d49230bbe5b7dc68708af181b7f
ngot23 <levuquang2k1@gmail.com>
2021-02-23 11:17:21 +0700

    perfect version with full test

--- test/StakBank.js
+++ test/StakBank.js
@@ -11,44 +11,58 @@ function Delay(mili) {
 }
 
 describe("Stakbank contract", function() {
-    const eth = BigNumber.from("1000000000000000000");
-    let Stakbank;
-    let JSTCoin;
-    let owner, address1, address2, address3, address4, moneyAddress;
-    let addresses;
-    let miliTime = 60000;
-    let feePerCoin = 100;
+    const e18 = BigNumber.from("1000000000000000000");
+    let stakBank;
+    let jstToken;
+    let owner, address1, address2, address3, address4, moneyAddress, addresses;
+    let periodTime;
+    let feeUnitPercent;
+    let minAmountToStake;
 
     before(async function() {
         [owner, address1, address2, address3, address4, moneyAddress, ...addresses] = await ethers.getSigners();
-        let JSTCoinFact = await ethers.getContractFactory("JSTCoinTest2");
-        JSTCoin = await JSTCoinFact.deploy(1000);
-        let StakbankFact = await ethers.getContractFactory("StakbankTest2");
-        Stakbank = await StakbankFact.deploy(JSTCoin.address);
-        await JSTCoin.verifyStakbank(Stakbank.address);
+        let JST = await ethers.getContractFactory("JST");
+        jstToken = await JST.deploy(ethers.utils.parseUnits("100", 18));
+        let StakBank = await ethers.getContractFactory("StakBank");
+        stakBank = await StakBank.deploy(jstToken.address);
+        await jstToken.verifyStakBank(stakBank.address);
         console.log(`owner: ${owner.address} | address1: ${address1.address} | address2: ${address2.address} | address3: ${address3.address}`);
+        periodTime = await stakBank.periodTime();
+        feeUnitPercent = await stakBank.feeUnitPercent();
+        minAmountToStake = await stakBank.minAmountToStake();
     });
     
-    describe("Owner test", function() {
+    describe("Owner test and Authorize stakBank", function() {
         it("Should set to the right owner", async function() {
-            expect(await JSTCoin.owner()).to.equal(owner.address);
+            expect(await jstToken.owner()).to.equal(owner.address);
         });
 
-        it("Should JSTCoin and Stakbank have the same owner", async function() {
-            expect(await JSTCoin.owner()).to.equal(await Stakbank.owner());
+        it("Should jstToken and stakBank have the same owner", async function() {
+            expect(await jstToken.owner()).to.equal(await stakBank.owner());
+        });
+
+        it("Should verify stakBank successfully", async function() {
+            expect(await jstToken.StakBank()).to.equal(stakBank.address);
         });
     });
 
-    describe("Set up JST token", function() {
-        it("Should user1, 2, 3 get the JST token", async function() {
-            await JSTCoin.transfer(address1.address, 200);
-            await JSTCoin.transfer(address2.address, 200);
-            await JSTCoin.transfer(address3.address, 200);
-            await JSTCoin.transfer(address4.address, 200);
-            expect(await JSTCoin.balanceOf(address1.address)).to.equal(200);
-            expect(await JSTCoin.balanceOf(address2.address)).to.equal(200);
-            expect(await JSTCoin.balanceOf(address3.address)).to.equal(200);
-            expect(await JSTCoin.balanceOf(address4.address)).to.equal(200);
+    describe("Setup JST", function() {
+        it("Should user1, 2, 3, 4 get JST", async function() {
+            await jstToken.transfer(address1.address, e18.mul(10));
+            await jstToken.transfer(address2.address, e18.mul(10));
+            await jstToken.transfer(address3.address, e18.mul(10));
+            await jstToken.transfer(address4.address, e18.mul(10));
+            expect(await jstToken.balanceOf(address1.address)).to.equal(e18.mul(10));
+            expect(await jstToken.balanceOf(address2.address)).to.equal(e18.mul(10));
+            expect(await jstToken.balanceOf(address3.address)).to.equal(e18.mul(10));
+            expect(await jstToken.balanceOf(address4.address)).to.equal(e18.mul(10));
+        });
+
+        it("Should setup allowance", async function() {
+            await (await jstToken.connect(address1)).approve(stakBank.address, e18.mul(10));
+            await (await jstToken.connect(address2)).approve(stakBank.address, e18.mul(10));
+            await (await jstToken.connect(address3)).approve(stakBank.address, e18.mul(10));
+            await (await jstToken.connect(address4)).approve(stakBank.address, e18.mul(10));        
         });
     });
 
@@ -62,109 +76,106 @@ describe("Stakbank contract", function() {
         return ret;
     }    
 
-    describe("timeline1", function() {
-        it("Should user2 stake 50 after user1 stake 10 => 20s && Should owner increase balance and user1, 2 decrease", async function() {
-            let feeUser1 = 10 * feePerCoin;
-            let feeUser2 = 50 * feePerCoin;
-            let before = [], after = [];
-            before = await balance([owner, address1, address2]);
-            await (await Stakbank.connect(address1)).stake(10, {value: feeUser1});
-            Delay(20*1000);
-            await (await Stakbank.connect(address2)).stake(50, {value: feeUser2});
-            after = await balance([owner, address1, address2]);
-            console.log('Balance user1 increase: ' + (after[1].sub(before[1])));            
-            console.log('Balance user2 increase: ' + (after[2].sub(before[2])));
-            expect(await Stakbank.stakingOf(address1.address)).to.equal(10);
-            expect(await Stakbank.stakingOf(address2.address)).to.equal(50);
-        });
-        
-        it("Should moneyaddress transfer money to the Stakbank", async function() {
-            let before = [];
-            before = await balance([Stakbank]);
-            await address3.sendTransaction({
-                to: Stakbank.address,
-                value: eth.mul(60)
-            });
-            let after = [];
-            after = await balance([Stakbank]);
-            expect(after[0].sub(before[0])).to.equal(eth.mul(60));
-        });
+    function calcFee(amount) {
+        return (amount * feeUnitPercent) / 100;
+    }
 
-        it("Should distribute t1", async function() {
-            let before = [], after = [];
-            before = await balance([address1, address2]);
-            let countdown = await Stakbank.nextDistribution();
-            Delay(countdown * 1000);
-            await Stakbank.rewardDistribution();
-            await (await Stakbank.connect(address1)).withdrawReward();
-            await (await Stakbank.connect(address2)).withdrawReward();
-            after = await balance([address1, address2]);
+    describe("timeline1", function() {
+        it("Should user2 stake 60000 after user1 stake 10000 => 20s", async function() {
+            await (await stakBank.connect(address1)).stake(10000, {value: calcFee(10000)});
+            Delay(20*1000);
+            await (await stakBank.connect(address2)).stake(60000, {value: calcFee(60000)});
+
+            expect(await stakBank.stakingOf(address1.address)).to.equal(10000);
+            expect(await stakBank.stakingOf(address2.address)).to.equal(60000);
         });
         
-        it("Should user1-30coin 5s user2-10coin 5s user3-60coin", async function() {
-            await moneyAddress.sendTransaction({
-                to: Stakbank.address,
-                value: eth.mul(100)
+        it("Should distribute t1", async function() {
+            let before = [];
+            before = await balance([stakBank]);
+
+            await address3.sendTransaction({
+                to: stakBank.address,
+                value: e18.mul(60)
             });
+
+            let after = [];
+            after = await balance([stakBank]);
+            expect(after[0].sub(before[0])).to.equal(e18.mul(60));
+
+            let countdown = await stakBank.nextDistribution();
+            Delay(countdown * 1000);
+            await stakBank.rewardDistribution();
+        });
+        
+        it("Should user1-300000coin 5s user2-15000coin 5s user3-70000coin", async function() {
             let before = [];
             before = await balance([owner]);
-            await (await Stakbank.connect(address1)).stake(30, {value: 30 * feePerCoin});
+            await (await stakBank.connect(address1)).stake(300000, {value: calcFee(300000)});
             Delay(5000);
-            await (await Stakbank.connect(address2)).stake(10, {value: 10 * feePerCoin});
+            await (await stakBank.connect(address2)).stake(15000, {value: calcFee(15000)});
             Delay(5000);
-            await (await Stakbank.connect(address3)).stake(60, {value: 60 * feePerCoin});
-            expect(await Stakbank.stakingOf(address1.address)).to.equal(40);
-            expect(await Stakbank.stakingOf(address2.address)).to.equal(60);
-            expect(await Stakbank.stakingOf(address3.address)).to.equal(60);
+            await (await stakBank.connect(address3)).stake(70000, {value: calcFee(70000)});
+            
+            expect(await stakBank.stakingOf(address1.address)).to.equal(300000 + 10000);
+            expect(await stakBank.stakingOf(address2.address)).to.equal(15000 + 60000);
+            expect(await stakBank.stakingOf(address3.address)).to.equal(70000);
             let after = [];
             after = await balance([owner]);
-            expect(after[0].sub(before[0])).to.equal((30 + 10 + 60) * feePerCoin);
+            expect(after[0].sub(before[0])).to.equal(calcFee(300000 + 15000 + 70000));
         });
 
         it("Shoud distribute t2", async function() {
             let before = [];
-            before = await balance([address1, address2, address3]);
-            let countdown = await Stakbank.nextDistribution();
-            Delay(countdown * 1000);
-            await Stakbank.rewardDistribution();
+            before = await balance([stakBank]);
+
+            await moneyAddress.sendTransaction({
+                to: stakBank.address,
+                value: e18.mul(100)
+            });
+
             let after = [];
-            after = await balance([address1, address2, address3]);
-            // await (await Stakbank.connect(address1)).withdrawReward();
-            // await (await Stakbank.connect(address2)).withdrawReward();
-            // await (await Stakbank.connect(address3)).withdrawReward();
+            after = await balance([stakBank]);
+            expect(after[0].sub(before[0])).to.equal(e18.mul(100));
+
+            let countdown = await stakBank.nextDistribution();
+            Delay(countdown * 1000);
+            await stakBank.rewardDistribution();
         });
 
-        it("Should user4-stake-unstake && user1-rut1 && user2-rut0", async function() {
-            let before = []
-            before = await balance([owner]);
-            await (await Stakbank.connect(address4)).stake(20, {value: 20 * feePerCoin});
-            let after = [];
-            after = await balance([owner]);
-            expect(after[0].sub(before[0])).to.equal(20 * feePerCoin);
-            await (await Stakbank.connect(address4)).unstakeAll();
-            await (await Stakbank.connect(address1)).unstakeWithId(2);
-            await (await Stakbank.connect(address2)).unstakeWithId(1);
-            expect(await Stakbank.stakingOf(address1.address)).to.equal(10);
-            expect(await Stakbank.stakingOf(address2.address)).to.equal(10);
+        it("Should user4-stake-unstake && user1-rut2 && user2-rut1", async function() {
+            let before = []; before = await balance([owner]);
+
+            await (await stakBank.connect(address4)).stake(20000000, {value: calcFee(20000000)});
+            
+            let after = []; after = await balance([owner]);
+
+            expect(after[0].sub(before[0])).to.equal(calcFee(20000000));
+
+            await (await stakBank.connect(address4)).unstakeAll();
+            await (await stakBank.connect(address1)).unstakeWithId(2);
+            await (await stakBank.connect(address2)).unstakeWithId(1);
+
+            expect(await stakBank.stakingOf(address1.address)).to.equal(10000);
+            expect(await stakBank.stakingOf(address2.address)).to.equal(15000);
         });
 
         it("Shoud t3 distribute", async function() {
             await moneyAddress.sendTransaction({
-                to: Stakbank.address,
-                value: eth.mul(80)
+                to: stakBank.address,
+                value: e18.mul(80)
             });
-            let before = [];
-            before = balance([address1, address2, address3, address4]);
-            let countdown = await Stakbank.nextDistribution();
+
+            let countdown = await stakBank.nextDistribution();
             Delay(countdown * 1000);
-            await Stakbank.rewardDistribution();
-            await (await (Stakbank.connect(address1))).unstakeAll();
-            await (await (Stakbank.connect(address2))).unstakeAll();
-            await (await (Stakbank.connect(address3))).unstakeAll();
-            let after = [];
-            after = await balance([address1, address2, address3, address4]);
+            await stakBank.rewardDistribution();
+
+            await (await (stakBank.connect(address1))).unstakeAll();
+
+            await (await (stakBank.connect(address2))).unstakeAll();
+
+            await (await (stakBank.connect(address3))).withdrawReward();
+            await (await (stakBank.connect(address3))).unstakeAll();
         });
     });
-
-
 });
--- testJS/test.js
+++ testJS/test.js
@@ -1,36 +1,42 @@
 const {BigNumber} = require("ethers");
 
-let stake1 = [BigNumber.from("10"), BigNumber.from("30")];
-let timestake1 = [BigNumber.from("1613817976"), BigNumber.from("1613818034")];
-let stake2 = [BigNumber.from("50"), BigNumber.from("10")];
-let timestake2 = [BigNumber.from("1613817996"), BigNumber.from("1613818039")];
-let stake3 = [BigNumber.from("60")];
-let timestake3 = [BigNumber.from("1613818044")];
-let distime = [BigNumber.from("1613818030"), BigNumber.from("1613818091"), BigNumber.from("1613818151")];
-let eth = [BigNumber.from("60000000000000000000"), BigNumber.from("100000000000000001600"), BigNumber.from("80000000000000003000")];
+let stake1 = [BigNumber.from("10000"), BigNumber.from("300000")];
+let timestake1 = [BigNumber.from("1614050322"), BigNumber.from("1614050373")];
 
-// test 1 
-let totaltime = (stake1[0].mul(61)).add((distime[1].sub(timestake1[1])).mul(stake1[1]))
-                .add(stake2[0].mul(61)).add((distime[1].sub(timestake2[1])).mul(stake2[1]))
-                .add((distime[1].sub(timestake3[0])).mul(stake3[0]));
-let unitvalue = eth[1].div(totaltime);
-console.log(totaltime.toString());
-let rw1_1 = unitvalue.mul(stake1[0].mul(61));
-let rw1_2 = unitvalue.mul((distime[1].sub(timestake1[1])).mul(stake1[1]));
-let rw2_1 = unitvalue.mul(stake2[0].mul(61));
-let rw2_2 = unitvalue.mul((distime[1].sub(timestake2[1])).mul(stake2[1]));
-let rw3 = unitvalue.mul((distime[1].sub(timestake3[0])).mul(stake3[0]));
-console.log(`user1 rut 2: ${rw1_2}`);
-console.log(`user2 rut 1: ${rw2_1}`);
+let stake2 = [BigNumber.from("60000"), BigNumber.from("15000")];
+let timestake2 = [BigNumber.from("1614050342"), BigNumber.from("1614050378")];
+
+let stake3 = [BigNumber.from("70000")];
+let timestake3 = [BigNumber.from("1614050383")];
+
+let distime = [BigNumber.from("1614050372"), BigNumber.from("1614050432"), BigNumber.from("1614050492")];
+
+let eth = [BigNumber.from("60000000000000000000"), BigNumber.from("100000000000000300000"), BigNumber.from("80000000000007820000")];
+
+let unitValue = [0, 0, 0, 0, 0, 0, 0];
+let totalTime = [0, 0, 0, 0, 0, 0, 0];
+
+totalTime[1] = (stake1[0].mul(distime[0].sub(timestake1[0])))
+                .add(stake2[0].mul(distime[0].sub(timestake2[0])));
+unitValue[1] = eth[0].div(totalTime[1]);
+
+totalTime[2] = (stake1[1].mul(distime[1].sub(timestake1[1])))
+                .add(stake2[1].mul(distime[1].sub(timestake2[1])))
+                .add(stake3[0].mul(distime[1].sub(timestake3[0])))
+                .add((stake1[0].add(stake2[0])).mul(60));
+
+unitValue[2] = eth[1].div(totalTime[2]);
 
 
-//test2
-let totaltime2 = (stake1[0].add(stake2[1]).add(stake3[0])).mul(60);
-let unitvalue2 = eth[2].div(totaltime2);
-console.log(totaltime2.toString());
-let money1 = (unitvalue2.mul(stake1[0]).mul(60)).add(rw1_1);
-let money2 = (unitvalue2.mul(stake2[1]).mul(60)).add(rw2_2);
-let money3 = (unitvalue2.mul(stake3[0]).mul(60)).add(rw3);
-console.log(`money1: ${money1}`);
-console.log(`money2: ${money2}`);
-console.log(`money3: ${money3}`);
+// user1 rut 2 - user 2 rut 1
+totalTime[3] = (stake1[0].add(stake2[1]).add(stake3[0])).mul(60);
+unitValue[3] = eth[2].div(totalTime[3]);
+
+
+console.log(BigNumber.from("58947368421055800000")
+            .add(BigNumber.from("13121652639631760000"))
+            .sub(BigNumber.from("72069021060687560000"))
+            .toString()
+);
+
+console.log(stake3[0].mul(60).mul(unitValue[3]).sub(BigNumber.from("58947368421055800000")).toString());
